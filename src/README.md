7cab91625b4691d6f0abda6bc4494198eb476af3
CVOvidiu <cvovidiu2002@gmail.com>
2023-01-24 13:31:38 +0200

    README && Meals component

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# Food Order App - ReactJS Project
+
+This is a course project from "React - The Complete Guide" by Maximilian Schwarzmüller.
+
+## Concepts used
+
+- Components
+- Styled components
+- Fragment
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,19 @@
+import classes from "./MealsSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus saepe
+        officiis, voluptatem reiciendis ut officia!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam,
+        laudantium voluptate at iusto cumque veniam!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
